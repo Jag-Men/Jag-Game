180caaa63818d2a267364b0749337021a59c3285
Lunass <lunassisbestpone@gmail.com>
2016-04-27 10:58:12 -0600

    hoase

---
+++ ChaoWorld2/ChaoWorld2/Entities/Arrow.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ChaoWorld2.Entities
+{
+  public class Arrow : Entity
+  {
+    void arrowmove(bool brool) 
+    {
+      if (brool == true)
+      {
+
+      }
+    }
+    bool groolean;
+    int jagxcache;
+    int jagycache;
+    public override void Update(GameTime gameTime)
+    {
+      base.Update(gameTime);
+      if (Mouse.GetState().LeftButton == ButtonState.Pressed)
+      {
+        groolean = true;
+
+      }
+
+
+    }
+    public override void Draw(SpriteBatch spriteBatch)
+    {
+      if (groolean == true)
+      spriteBatch.Draw(ContentLibrary.Sprites["arrow"], new Vector2(0, 0), Color.White);
+    }
+  }
+}
